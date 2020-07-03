c6d286f3e87fabfabf73f6a1f8770fcdf5aa8e21
Chase <chase@csh.rit.edu>
2020-07-03 17:39:03 -0400

    Added tags to pets

--- src/main/kotlin/com/petscape/server/models/GameTag.kt
+++ src/main/kotlin/com/petscape/server/models/GameTag.kt
@@ -1,5 +1,5 @@
 package com.petscape.server.models
 
 enum class GameTag {
-    skilling, boss, pvm, raids
+    skilling, boss, pvm, raids, minigame, other, wildy, slayer
 }
--- src/main/kotlin/com/petscape/server/models/Pet.kt
+++ src/main/kotlin/com/petscape/server/models/Pet.kt
@@ -1,5 +1,7 @@
 package com.petscape.server.models
 
+import com.petscape.server.models.GameTag.*
+
 enum class Pet(val display: String,
                val tags: List<GameTag>,
     //val boss: ?,
@@ -8,322 +10,322 @@ enum class Pet(val display: String,
 
     abyssalOrphan(
         display = "Abyssal Orphan",
-        tags = listOf(),
+        tags = listOf(pvm, boss, slayer),
         //boss = null,
         dropRate = 2560,
         image = "/pets/Abyssal Orphan.gif"
     ),
     babyChinchompa(
         display = "Baby Chinchompa",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 26_068_808,
         image = "/pets/Baby Chinchompa.gif"
     ),
     babyMole(
         display = "Baby Mole",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Baby Mole.gif"
     ),
     beaver(
         display = "Beaver",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 24_975_270,
         image = "/pets/Beaver.gif"
     ),
     bloodhound(
         display = "Bloodhound",
-        tags = listOf(),
+        tags = listOf(other),
         //boss = null,
         dropRate = 1000,
         image = "/pets/Bloodhound.gif"
     ),
     callistoCub(
         display = "Callisto Cub",
-        tags = listOf(),
+        tags = listOf(pvm, boss, wildy),
         //boss = null,
         dropRate = 2000,
         image = "/pets/Callisto Cub.gif"
     ),
     chaosElemental(
         display = "Chaos Elemental",
-        tags = listOf(),
+        tags = listOf(pvm, boss, wildy),
         //boss = null,
         dropRate = 300,
         image = "/pets/Chaos Elemental.gif"
     ),
     chompyChick(
         display = "Chompy Chick",
-        tags = listOf(),
+        tags = listOf(other),
         //boss = null,
         dropRate = 500,
         image = "/pets/Chompy Chick.gif"
     ),
     dagannothPrime(
         display = "Dagannoth Prime",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 5000,
         image = "/pets/Dagannoth Prime.gif"
     ),
     dagannothRex(
         display = "Dagannoth Rex",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 5000,
         image = "/pets/Dagannoth Rex.gif"
     ),
     dagannothSupreme(
         display = "Dagannoth Supreme",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 5000,
         image = "/pets/Dagannoth Supreme.gif"
     ),
     darkCore(
         display = "Dark Core",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 5000,
         image = "/pets/Dark Core.gif"
     ),
     generalGraardor(
         display = "General Graardor",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 5000,
         image = "/pets/General Graardor.gif"
     ),
     giantSquirrel(
         display = "Giant Squirrel",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 16_953_400,
         image = "/pets/Giant Squirrel.gif"
     ),
     hellpuppy(
         display = "Hellpuppy",
-        tags = listOf(),
+        tags = listOf(pvm, boss, slayer),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Hellpuppy.gif"
     ),
     herbi(
         display = "Herbi",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 6500,
         image = "/pets/Herbi.gif"
     ),
     heron(
         display = "Heron",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 17_298_423,
         image = "/pets/Heron.gif"
     ),
     ikkleHydra(
         display = "Ikkle Hydra",
-        tags = listOf(),
+        tags = listOf(pvm, boss, slayer),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Ikkle Hydra.gif"
     ),
     jalNibRek(
         display = "Jal-Nib-Rek",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 100,
         image = "/pets/Jal-Nib-Rek.gif"
     ),
     krilTsutsaroth(
         display = "K'ril Tsutsaroth",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 5000,
         image = "/pets/K'ril Tsutsaroth.gif"
     ),
     kalphitePrincess(
         display = "Kalphite Princess",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Kalphite Princess.gif"
     ),
     kraken(
         display = "Kraken",
-        tags = listOf(),
+        tags = listOf(pvm, boss, slayer),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Kraken.gif"
     ),
     kreeArra(
         display = "Kree'arra",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 5000,
         image = "/pets/Kree'arra.gif"
     ),
     lilZik(
         display = "Lil' Zik",
-        tags = listOf(),
+        tags = listOf(pvm, raids),
         //boss = null,
         dropRate = 650,
         image = "/pets/Lil' Zik.gif"
     ),
     LittleNightmare(
         display = "Little Nightmare",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 4000,
         image = "/pets/Little Nightmare.gif"
     ),
     noon(
         display = "Noon/Midnight",
-        tags = listOf(),
+        tags = listOf(pvm, boss, slayer),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Noon/Midnight.gif"
     ),
     olmlet(
         display = "Olmlet",
-        tags = listOf(),
+        tags = listOf(pvm, raids),
         //boss = null,
         dropRate = 1765,
         image = "/pets/Olmlet.gif"
     ),
     penanceQueen(
         display = "Penance Queen",
-        tags = listOf(),
+        tags = listOf(minigame),
         //boss = null,
         dropRate = 1000,
         image = "/pets/Penance Queen.gif"
     ),
     phoenix(
         display = "Phoenix",
-        tags = listOf(),
+        tags = listOf(minigame),
         //boss = null,
         dropRate = 5000,
         image = "/pets/Phoenix.gif"
     ),
     princeBlackDragon(
         display = "Prince Black Dragon",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Prince Black Dragon.gif"
     ),
     riftGaurdian(
         display = "Rift Gaurdian",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 17_591_116,
         image = "/pets/Rift Gaurdian.gif"
     ),
     rockGolem(
         display = "Rock Golem",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 18_893_148,
         image = "/pets/Rock Golem.gif"
     ),
     rocky(
         display = "Rocky",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 17_851_931,
         image = "/pets/Rocky.gif"
     ),
     scorpiasOffspring(
         display = "Scorpia's Offspring",
-        tags = listOf(),
+        tags = listOf(pvm, boss, wildy),
         //boss = null,
         dropRate = 2016,
         image = "/pets/Scorpia's Offspring.gif"
     ),
     skotos(
         display = "Skotos",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 65,
         image = "/pets/Skotos.gif"
     ),
     smokeDevil(
         display = "Smoke Devil",
-        tags = listOf(),
+        tags = listOf(pvm, boss, slayer),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Smoke Devil.gif"
     ),
     smolcano(
         display = "Smolcano",
-        tags = listOf(),
+        tags = listOf(minigame),
         //boss = null,
         dropRate = 4000,
         image = "/pets/Smolcano.gif"
     ),
     snakeling(
         display = "Snakeling",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 4000,
         image = "/pets/Snakeling.gif"
     ),
     srarcha(
         display = "Srarcha",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Srarcha.gif"
     ),
     tangleroot(
         display = "Tangleroot",
-        tags = listOf(),
+        tags = listOf(skilling),
         //boss = null,
         dropRate = 60_000_000,
         image = "/pets/Tangleroot.gif"
     ),
     tzrekJad(
         display = "Tzrek-Jad",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 100,
         image = "/pets/Tzrek-Jad.gif"
     ),
     venenatisSpiderling(
-        display = "Venenatis SPiderling",
-        tags = listOf(),
+        display = "Venenatis Spiderling",
+        tags = listOf(pvm, boss, wildy),
         //boss = null,
         dropRate = 2000,
         image = "/pets/Venenatis SPiderling.gif"
     ),
     vetionJr(
         display = "Vet'ion Jr.",
-        tags = listOf(),
+        tags = listOf(pvm, boss, wildy),
         //boss = null,
         dropRate = 2000,
         image = "/pets/Vet'ion Jr..gif"
     ),
     vorki(
         display = "Vorki",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 3000,
         image = "/pets/Vorki.gif"
     ),
     youngllef(
         display = "Youngllef",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 800,
         image = "/pets/Youngllef.gif"
     ),
     zilyana(
         display = "Zilyana",
-        tags = listOf(),
+        tags = listOf(pvm, boss),
         //boss = null,
         dropRate = 5000,
         image = "/pets/Zilyana.gif"
